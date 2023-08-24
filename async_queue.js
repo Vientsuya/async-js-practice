bca28d46b8bbd63a4a7ef761df7be5c6119dd110
Marcin Dzierwa <vientvotredouleur@gmail.com>
2023-08-24 18:36:27 +0000

    tasks are now done in parallel with a set number of workers

--- async_queue.js
+++ async_queue.js
@@ -5,31 +5,42 @@ function doubleNum(num, time) {
 }
 
 const tasks = [
-  () => doubleNum(20, 1300),
-  () => doubleNum(8, 1000),
-  () => doubleNum(17, 2200),
-  () => doubleNum(34, 900),
-  () => doubleNum(72, 3000),
-  () => doubleNum(4, 600),
+  () => doubleNum(8, 5000),
+  () => doubleNum(17, 8000),
+  () => doubleNum(34, 10000),
+  () => doubleNum(72, 6000),
+  () => doubleNum(3, 4000),
+  () => doubleNum(6, 8000),
+  () => doubleNum(99, 3000),
+  () => doubleNum(328, 7000),
+  () => doubleNum(883, 12000),
+  () => doubleNum(333, 6000),
 ];
 
-function createQueue(tasks, maxNumOfWorkers) {
+function createQueue(tasks, maxNumOfWorkers = 4) {
   let taskIndex = 0;
+  let numOfWorkers = 0;
 
   return new Promise((resolve) => {
     const handleResult = (index) => (result) => {
       console.log(`Task number: ${index + 1} finished with result: ${result}`);
       tasks[index] = result;
-      taskIndex++;
+      numOfWorkers--;
+      console.log(`Number of workers: ${numOfWorkers}/${maxNumOfWorkers}`);
       getNextTask();
     };
 
     const getNextTask = () => {
-      if (taskIndex < tasks.length) {
+      if (numOfWorkers < maxNumOfWorkers && taskIndex < tasks.length) {
         tasks[taskIndex]()
           .then(handleResult(taskIndex))
           .catch(handleResult(taskIndex));
-      } else {
+        taskIndex++;
+        numOfWorkers++;
+        console.log(`Starting new task number: ${taskIndex}`);
+        console.log(`Number of workers: ${numOfWorkers}/${maxNumOfWorkers}`);
+        getNextTask();
+      } else if (numOfWorkers === 0 && taskIndex === tasks.length) {
         console.log("All tasks done successfully");
         resolve(tasks);
       }
@@ -38,4 +49,4 @@ function createQueue(tasks, maxNumOfWorkers) {
   });
 }
 
-createQueue(tasks, 0);
+createQueue(tasks);
