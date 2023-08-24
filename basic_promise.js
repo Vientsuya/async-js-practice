63c65a8542ec77bdd914af6ee99077ce0df0f965
Marcin Dzierwa <37550706+Vientsuya@users.noreply.github.com>
2023-08-24 16:37:49 +0000

    Delete basic_promise.js

--- basic_promise.js
+++
@@ -1,9 +0,0 @@
-function returnNum(num, time) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(num * 2);
-    }, time);
-  });
-}
-
-returnNum(5, 1000).then((val) => console.log(val));
